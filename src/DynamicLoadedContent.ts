d6d5faae4a53ac7089274dacb1f726e20739d954
Marc Emmanuel <marc.emmanuel@virtual-identity.com>
2021-07-09 08:55:43 +0100

    feat: 🎸 Allow loading by script tag

--- src/DynamicLoadedContent.ts
+++ src/DynamicLoadedContent.ts
@@ -1,10 +1,12 @@
 export class DynamicLoadedContent extends HTMLElement {
   imports: string[] = [];
   html: string = '';
+  useImports: boolean = true;
 
   protected async connectedCallback() {
     const baseUrl: string = this.getAttribute('base-url') || '';
-    await Promise.all(this.imports.map((url: string) => import(baseUrl + url)));
+      await Promise.all(this.imports.map((url: string) => this.useImports ? import(baseUrl + url) : this.loadScriptSync(baseUrl + url)));
+
     this.render();
   }
 
@@ -15,5 +17,16 @@ export class DynamicLoadedContent extends HTMLElement {
 
   protected componentDidLoad() {
     // Add runtime code here
+  }
+
+  private loadScriptSync(src: string): Promise<void> {
+    return new Promise((res, rej) => {
+      var s = document.createElement('script');
+      s.src = src;
+      s.type = "text/javascript";
+      s.async = false;
+      s.onload = () => res();
+      document.head.appendChild(s);
+    })
   }
 }
